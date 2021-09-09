5864de5c57139b52f24150f92a9c48174e02f8f1
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-09-09 23:34:08 +0100

    Create `Notification` component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,9 @@
+function Notification({ text }) {
+  return (
+    <div className="notification-card light-shadow center">
+      <h1>{text}</h1>
+    </div>
+  );
+}
+
+export default Notification;
